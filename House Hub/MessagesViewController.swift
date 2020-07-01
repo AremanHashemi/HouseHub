3739ed01f688cd6e9d228d38ab6adbf98641c40f
humza raza <hsraza@ucsc.edu>
2020-06-30 19:18:30 -0700

    login/signup/creategrp_skeleton

--- House Hub/MessagesViewController.swift
+++ House Hub/MessagesViewController.swift
@@ -8,4 +8,11 @@
 
 import UIKit
 class MessagesViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
 }
